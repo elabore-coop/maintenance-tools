661a5b597a943e5698f60ccec5a1620920449d33
clementthomas <clement@cotyl.net>
2024-04-09 11:24:52 +0200

    [IMP] maintenance_server_monitoring_maintenance_equipment_status: * bugfix

--- maintenance_server_monitoring_maintenance_equipment_status/models/maintenance_equipment.py
+++ maintenance_server_monitoring_maintenance_equipment_status/models/maintenance_equipment.py
@@ -7,17 +7,17 @@ class MaintenanceEquipment(models.Model):
     def create_maintenance_request(self, error_level, description):
         res = super(MaintenanceEquipment, self).create_maintenance_request(error_level, description)
         if self.error_maintenance_request:
-            error_status = self.env["maintenance.equipment.status"].search([("is_error_status",'=',True),'|', ('category_ids', 'in', [self.category_id.id]), ('category_ids', '=', False)])
+            error_status = self.env["maintenance.equipment.status"].search([("is_error_status",'=',True),'|', ('category_ids', 'in', [self.category_id.id]), ('category_ids', '=', False)], limit=1)
             if error_status:
                 self.status_id = error_status
         else:
-            warning_status = self.env["maintenance.equipment.status"].search([("is_warning_status",'=',True),'|', ('category_ids', 'in', [self.category_id.id]), ('category_ids', '=', False)])
+            warning_status = self.env["maintenance.equipment.status"].search([("is_warning_status",'=',True),'|', ('category_ids', 'in', [self.category_id.id]), ('category_ids', '=', False)], limit=1)
             if warning_status:
                 self.status_id = warning_status
         return res
 
     def no_error(self):
         res = super(MaintenanceEquipment, self).no_error()
-        ok_status = self.env["maintenance.equipment.status"].search([("is_error_status",'=',False),("is_warning_status",'=',False),'|', ('category_ids', 'in', [self.category_id.id]), ('category_ids', '=', False)])
+        ok_status = self.env["maintenance.equipment.status"].search([("is_error_status",'=',False),("is_warning_status",'=',False),'|', ('category_ids', 'in', [self.category_id.id]), ('category_ids', '=', False)], limit=1)
         self.status_id = ok_status
         return res
