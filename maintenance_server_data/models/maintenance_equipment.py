ff75a1b4cdceb9425e2722fcf5ab59e215555dd0
Boris Gallet <boris.gallet@gmail.com>
2025-05-13 17:45:50 +0200

    [IMP] add ``name_fr`` field to maintenance equipment

--- maintenance_server_data/models/maintenance_equipment.py
+++ maintenance_server_data/models/maintenance_equipment.py
@@ -1,17 +1,23 @@
-from odoo import fields, models
-
+from odoo import fields, models, api
 
 
 class MaintenanceEquipment(models.Model):
-    _inherit = 'maintenance.equipment'
+    _inherit = "maintenance.equipment"
 
-    server_ip = fields.Char('Server Ip Address')
-    distribution_id = fields.Many2one('os.distribution', string='Distribution')
-    service_ids = fields.One2many('service.instance', 'equipment_id', string='Services')
-    hosting_city = fields.Char('Hosting City')
-    nb_cores = fields.Integer('Nb Cores')
-    ram = fields.Integer('RAM (Go)')
-    disk_storage = fields.Integer('Disk Storage (Go)')
-    backup_activated = fields.Boolean('Backup Activated ?')
-    backup_server_id = fields.Many2one('backup.server', string='Backup Server')
-    backup_ok = fields.Boolean('Backup OK ?')
+    server_ip = fields.Char("Server Ip Address")
+    distribution_id = fields.Many2one("os.distribution", string="Distribution")
+    service_ids = fields.One2many("service.instance", "equipment_id", string="Services")
+    hosting_city = fields.Char("Hosting City")
+    nb_cores = fields.Integer("Nb Cores")
+    ram = fields.Integer("RAM (Go)")
+    disk_storage = fields.Integer("Disk Storage (Go)")
+    backup_activated = fields.Boolean("Backup Activated ?")
+    backup_server_id = fields.Many2one("backup.server", string="Backup Server")
+    backup_ok = fields.Boolean("Backup OK ?")
+
+    name_fr = fields.Char("Name (FR)", compute="_compute_name_fr", store=True)
+
+    @api.depends("name")
+    def _compute_name_fr(self):
+        for record in self:
+            record.name_fr = record.with_context(lang="fr_FR").name
